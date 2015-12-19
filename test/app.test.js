0dccf6bf4fa841efdae81a54be9df9eaf6496d72
William Henshaw <whenshaw87@googlemail.com>
2015-12-18 19:23:08 -0800

    Test stuff

--- test/app.test.js
+++ test/app.test.js
@@ -9,7 +9,7 @@ var module = angular.module('Grasp', [
   'ngRoute',
   'ngMaterial',
   'Grasp.Choice'
-])
+]);
 
 describe('Code blocks', function () {
     beforeEach(angular.mock.module('Grasp'));
@@ -27,26 +27,6 @@ describe('Code blocks', function () {
                 var array = scope.codeBlocks[1];
                 scope.droppedCodeBlocks.push(array);
                 expect(scope.droppedCodeBlocks.length).to.deep.equal(1);
-            }))
+            }));
         });
-  //  it('test it2', function() {
-  //   // expect(true).to.be(true);
-  //   expect('foo').to.be.a('string')
-  // });
-  //   it('sorts in descending order by default', function() {
-  //       var users = ['jack', 'igor', 'jeff'];
-  //       var sorted = sortUsers(users);
-  //   expect(sorted).toEqual(['jeff', 'jack', 'igor']);
-  // });
 });
-    // var Person;
-    // beforeEach(module('Joy'));
-    // beforeEach(inject(function (_Person_) {
-    //     Person = _Person_;
-    // }));
-
-    // describe('Constructor', function () {
-    //     it('assigns a name', function () {
-    //         expect(new Person('Ben')).to.have.property('name', 'Ben');
-    //     });
-    // });
