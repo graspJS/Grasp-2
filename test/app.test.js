99102c03af5f992f81592f01fa1ed735b7b77865
William Henshaw <whenshaw87@googlemail.com>
2015-12-20 15:11:15 -0800

    Added test

--- test/app.test.js
+++ test/app.test.js
@@ -26,5 +26,47 @@ describe('Code blocks', function () {
                 scope.droppedCodeBlocks.push(array);
                 expect(scope.droppedCodeBlocks.length).to.deep.equal(1);
             }));
+
+             it('should render code after code is dropped into droppedCodeBlocks', inject(function ($controller){
+                var scope = {};
+                ctrl = $controller('CanvasCTRL', { $scope: scope });
+                var array = scope.codeBlocks[1];
+                scope.onDrop(array);
+                expect(scope.code).to.deep.equal('var noNameArray = [];\n');
+            }));
         });
+      describe('Code block types test', function () {
+            it('Set the name of a variable', inject(function ($controller){
+              var scope = {};
+              ctrl = $controller('CanvasCTRL', { $scope: scope });
+              var variable = scope.codeBlocks[0];
+              scope.onDrop(variable);
+              var variable = scope.droppedCodeBlocks[0];
+              var event = document.createEvent("MouseEvent");
+              scope.setVariable(variable, "joe", 5, event);
+              expect(scope.droppedCodeBlocks[0].name).to.deep.equal('joe');
+            }));
+
+            it('Set the name of a variable', inject(function ($controller){
+              var scope = {};
+              ctrl = $controller('CanvasCTRL', { $scope: scope });
+              var object = scope.codeBlocks[2];
+              scope.onDrop(object);
+              var object = scope.droppedCodeBlocks[0];
+              object.setKey("Key1")
+              object.setValue("Value1")
+              expect(scope.droppedCodeBlocks[0].key).to.deep.equal('Key1');
+            }));
+
+            it('It drops a variable value in an array', inject(function ($controller){
+              var scope = {};
+              ctrl = $controller('CanvasCTRL', { $scope: scope });
+              var variable = scope.codeBlocks[0];
+              var array = scope.codeBlocks[1];
+              scope.onDrop(variable);
+              scope.onDrop(array);
+              scope.droppedCodeBlocks[1].value.push(variable);
+              expect(scope.droppedCodeBlocks[1].value.length).to.deep.equal(1);
+            }));
+      })
 });
