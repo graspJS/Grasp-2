e7cb9aaea0c398fbf64f61f727cbf0e7836ef948
William Henshaw <whenshaw87@googlemail.com>
2015-12-21 16:11:47 -0800

    Added more tests

--- test/app.test.js
+++ test/app.test.js
@@ -47,7 +47,7 @@ describe('Code blocks', function () {
               expect(scope.droppedCodeBlocks[0].name).to.deep.equal('joe');
             }));
 
-            it('Set the name of a variable', inject(function ($controller){
+            it('Set the name of an object', inject(function ($controller){
               var scope = {};
               ctrl = $controller('CanvasCTRL', { $scope: scope });
               var object = scope.codeBlocks[2];
@@ -58,7 +58,7 @@ describe('Code blocks', function () {
               expect(scope.droppedCodeBlocks[0].key).to.deep.equal('Key1');
             }));
 
-            it('It drops a variable value in an array', inject(function ($controller){
+            it('It should drop a variable value in an array', inject(function ($controller){
               var scope = {};
               ctrl = $controller('CanvasCTRL', { $scope: scope });
               var variable = scope.codeBlocks[0];
@@ -68,5 +68,82 @@ describe('Code blocks', function () {
               scope.droppedCodeBlocks[1].value.push(variable);
               expect(scope.droppedCodeBlocks[1].value.length).to.deep.equal(1);
             }));
-      })
+      });
+    describe('function tests', function () {
+            it('Calling add should add two values together', inject(function ($controller){
+              var scope = {};
+              ctrl = $controller('CanvasCTRL', { $scope: scope });
+              var variable = scope.codeBlocks[0];
+              var event = document.createEvent("MouseEvent");
+              scope.onDrop(variable);
+              scope.setVariable(scope.droppedCodeBlocks[0], "joe", 5, event);
+              var variable2 = scope.codeBlocks[0];
+              var event = document.createEvent("MouseEvent");
+              scope.onDrop(variable2);
+              scope.setVariable(scope.droppedCodeBlocks[1], "john", 8, event);
+              var add = scope.codeBlocks[3];
+              scope.onDrop(add);
+              scope.droppedCodeBlocks[2].addParam(scope.droppedCodeBlocks[0]);
+              scope.droppedCodeBlocks[2].addParam(scope.droppedCodeBlocks[1]);
+              expect(scope.droppedCodeBlocks[2].execute()).to.deep.equal(13);
+            }));
+
+           it('Calling multiply should multiply two values together', inject(function ($controller){
+              var scope = {};
+              ctrl = $controller('CanvasCTRL', { $scope: scope });
+              var variable = scope.codeBlocks[0];
+              var event = document.createEvent("MouseEvent");
+              scope.onDrop(variable);
+              scope.setVariable(scope.droppedCodeBlocks[0], "joe", 5, event);
+              var variable2 = scope.codeBlocks[0];
+              var event = document.createEvent("MouseEvent");
+              scope.onDrop(variable2);
+              scope.setVariable(scope.droppedCodeBlocks[1], "john", 3, event);
+              var multiply = scope.codeBlocks[5];
+              scope.onDrop(multiply);
+              scope.droppedCodeBlocks[2].addParam(scope.droppedCodeBlocks[0]);
+              scope.droppedCodeBlocks[2].addParam(scope.droppedCodeBlocks[1]);
+              expect(scope.droppedCodeBlocks[2].execute()).to.deep.equal(15);
+            }));
+
+            it('Calling divide should divide one value by the other', inject(function ($controller){
+              var scope = {};
+              ctrl = $controller('CanvasCTRL', { $scope: scope });
+              var variable = scope.codeBlocks[0];
+              var event = document.createEvent("MouseEvent");
+              scope.onDrop(variable);
+              scope.setVariable(scope.droppedCodeBlocks[0], "joe", 10, event);
+              var variable2 = scope.codeBlocks[0];
+              var event = document.createEvent("MouseEvent");
+              scope.onDrop(variable2);
+              scope.setVariable(scope.droppedCodeBlocks[1], "john", 5, event);
+              var divide = scope.codeBlocks[6];
+              scope.onDrop(divide);
+              scope.droppedCodeBlocks[2].addParam(scope.droppedCodeBlocks[0]);
+              scope.droppedCodeBlocks[2].addParam(scope.droppedCodeBlocks[1]);
+              expect(scope.droppedCodeBlocks[2].execute()).to.deep.equal(2);
+            }));
+
+            it('Should call a function on the reult of a dropped function', inject(function ($controller){
+              var scope = {};
+              ctrl = $controller('CanvasCTRL', { $scope: scope });
+              var variable = scope.codeBlocks[0];
+              var event = document.createEvent("MouseEvent");
+              scope.onDrop(variable);
+              scope.setVariable(scope.droppedCodeBlocks[0], "joe", 5, event);
+              var variable2 = scope.codeBlocks[0];
+              var event = document.createEvent("MouseEvent");
+              scope.onDrop(variable2);
+              scope.setVariable(scope.droppedCodeBlocks[1], "john", 8, event);
+              var add = scope.codeBlocks[3];
+              scope.onDrop(add);
+              scope.droppedCodeBlocks[2].addParam(scope.droppedCodeBlocks[0]);
+              scope.droppedCodeBlocks[2].addParam(scope.droppedCodeBlocks[1]);
+              var multiply = scope.codeBlocks[5];
+              scope.onDrop(multiply);
+              scope.droppedCodeBlocks[3].addParam(scope.droppedCodeBlocks[2]);
+              scope.droppedCodeBlocks[3].addParam(scope.droppedCodeBlocks[0]);
+              expect(scope.droppedCodeBlocks[3].execute()).to.deep.equal(65);
+            }));
+    })
 });
